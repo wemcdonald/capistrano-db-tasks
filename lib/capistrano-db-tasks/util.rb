5a183dc687f34a64a7ac89ab89a8cd28d89e38b5
Will McDonald <wemcdonald@gmail.com>
2015-01-22 15:29:08 -0800

    Allow skipping prompts so I can run this remotely (i.e. run on staging to pull from production)

--- lib/capistrano-db-tasks/util.rb
+++ lib/capistrano-db-tasks/util.rb
@@ -1,5 +1,6 @@
 module Util
   def self.prompt(msg, prompt = "(y)es, (n)o ")
+    return true if ENV['SKIP_CAPISTRANO_DB_PROMPT']
     answer = Capistrano::CLI.ui.ask("#{msg} #{prompt} ? ") do |q|
       q.overwrite = false
       q.validate = /^y$|^yes$|^n$|^no$/i
